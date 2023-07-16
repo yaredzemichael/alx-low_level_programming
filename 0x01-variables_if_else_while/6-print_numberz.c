7d4a29f88233e54099c7debb50323f592d2e9e76
Your Name <you@example.com>
2023-07-16 16:11:23 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+
+{
+
+        int x = 0;
+
+        while (x < 10)
+        {
+                printf("%d\n", x);
+                x++;
+        }
+        return (0);
+}
