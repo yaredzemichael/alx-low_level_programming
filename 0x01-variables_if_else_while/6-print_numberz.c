025ccd55bc8f75499e7c7ad50f4fb8f12c3d8543
Your Name <you@example.com>
2023-07-17 12:33:07 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
- *  Entry point
+ * main - prints all single digit numbers of base 10 starting from 0.
+ *        with out char 
  *
  * Return: Always 0 (Success)
  */
-
-
 int main(void)
-
 {
 
-        char x;
+        int x;
 
-        for (x = 0; x<10; x++)
-        {
-        putchar(x + '0');
-	}       
+        for (x = 0; x < 10; x++)
+                putchar((x % 10) + '0');
+
         putchar ('\n');
+
         return (0);
 }
