2fb484a02798cb5e49aef76991025be73397d9ba
Your Name <you@example.com>
2023-07-17 12:38:27 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,33 +3,33 @@
 #include <time.h>
 
 /**
+ * main - prints all possible combinationsof single-digit numbers.
  *
- * main - Entry point
- * Return: Always 0 (success)
+ * Return: Always 0.
  */
 
 int main(void)
 
 {
-	int d, p;
+        int d, p;
 
-	for (d = '0'; d< '9'; d++)
-	{
-	
-	for (p = d + 1; p <= '9'; p++)
-	{
-	if (p != d)
-	{
-	putchar(d);
-	putchar(p);
-	if (d == '8' && p == '9')
-	continue;
-	putchar(',');
-	putchar(' ');
+        for (d = '0'; d < '9'; d++)
+        {
+
+        for (p = d + 1; p <= '9'; p++)
+        {
+        if (p != d)
+        {
+        putchar(d);
+        putchar(p);
+        if (d == '8' && p == '9')
+        continue;
+        putchar(',');
+        putchar(' ');
         }
-	}
-	}
-	putchar('\n');
-	return (0);
+        }
+        }
+        putchar('\n');
+        return (0);
 }
 
