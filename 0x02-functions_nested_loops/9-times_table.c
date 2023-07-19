d5b1e20efafd857c9b3f75868460b37a21ee7e01
Your Name <you@example.com>
2023-07-19 11:00:27 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,28 +9,29 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-			k = j * i;
-			if (k > 9)
-			{
-				m = k % 10;
-				n = (k - m) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(n + '0');
-				_putchar(m + '0');
-			}
-				else
-				{
-					if (j !=0)
-					{
-						_putchar(44);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(k + '0');
-				}
-		}_putchar('\n');
+	for (j = 0; j <= 9; j++)
+	{
+	k = j * i;
+	if (k > 9)
+	{
+	m = k % 10;
+	n = (k - m) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(n + '0');
+	_putchar(m + '0');
+	}
+	else
+	{
+	if (j != 0)
+	{
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
+	}
+	_putchar(k + '0');
+	}
+	}
+	_putchar('\n');
 	}
 }
