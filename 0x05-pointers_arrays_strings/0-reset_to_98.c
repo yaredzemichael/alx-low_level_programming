3f1e3509dffa76154c43a23b461cb9a5fcd964a6
Your Name <you@example.com>
2023-07-24 11:02:19 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ * @n: an intiger input pointer 
+ * return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
