ab5938937591d2d130770a3e54152ca844063307
Your Name <you@example.com>
2023-07-24 12:25:09 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
+ */
+void puts2(char *str)
+{
+	int i; 
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
