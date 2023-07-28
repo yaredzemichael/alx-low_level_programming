1319205e09fcd8c6ae8beef8be3edc42c09ade31
Your Name <you@example.com>
2023-07-28 13:08:38 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,10 +1,37 @@
-#include <stdio.h>
+#include "stdio.h"
 
 /**
- * print_buffer - Print the entire buffer w/ certain conditions
- * @b: The buffer to print
- * @size: The size of the buffer
+ * rev_string - is indicate the reverse array
+ * @n: The buffer represent integer params
+ * return: (0);
  */
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+	
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+/**
+ * infinite_add - add two number together
+ * @n1: First number to add
+ * @n2: Second number to add
+ * r: pointer to buffer
+ * size_r: buffer size
+ */
+
 char *infnite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
@@ -16,12 +43,12 @@ char *infnite_add(char *n1, char *n2, char *r, int size_r)
 		j++;
 	i--;
 	j--;
-	if (j >= size_r || i >= size_r
+	if (j >= size_r || i >= size_r)
 			return (0);
 	while (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
-		voj1 = *(n1 + 1) - '0';
+		vol1 = *(n1 + 1) - '0';
 		else
 		vol1 = *(n1 + 1) = '0';
 		if (j < 0)
@@ -35,7 +62,7 @@ char *infnite_add(char *n1, char *n2, char *r, int size_r)
 		overflow = 0;
 		if (digits >= (size_r - 1))
 		return (0);
-		*(r + digits) - (temp_tot % 10) + '0';
+		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
 		j--;
 		i--;
