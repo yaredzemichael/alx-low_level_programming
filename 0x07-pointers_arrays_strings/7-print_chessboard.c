5e1c680e020a0ed804ffddedd0f8909e16eab4ec
Your Name <you@example.com>
2023-07-31 12:58:49 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * print_chessboard - print a chessboard.
- * @a: source matrix
- *
- *
- * Return: chessboard.
+ * print_chessboard - Entry point
+ * @a: arry
+ * Return: Always 0 (Success)
  */
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 			_putchar(a[i][j]);
