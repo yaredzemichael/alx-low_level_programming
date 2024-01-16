2ebdb36d7cb7076e6d832679c8f0876a713be9f3
yared zemichael <yaredzemichael@gmail.com>
2024-01-16 11:36:34 -0800

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup -
+ * @str:
+ * Return:
+ */
+
+char *_strdup(char *str)
+{
+	int a = 0, i = 1;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	s = malloc((sizeof(char) * i) + 1);
+	
+	if (s = NULL)
+			return (NULL);
+	while (a < i)
+	{
+		s[a] = str[a];
+	}
+
+	s[a] = '\0';
+	return (s);
+}
