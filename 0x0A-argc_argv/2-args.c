8a792601dcd1e809810243304dc1db9d84f1e565
yared zemichael <yaredzemichael@gmail.com>
2024-01-17 10:22:23 -0800

    2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -
+ * @argc:
+ * @argv:
+ *
+ * Return:
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
