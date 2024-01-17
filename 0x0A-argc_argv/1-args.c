77522d5ac470b6a36f21a5748aa390298fa1c552
yared zemichael <yaredzemichael@gmail.com>
2024-01-17 10:13:41 -0800

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -
+ * @argc:
+ * @argv:
+ *
+ * Return:
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
